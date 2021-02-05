1e49ee466d41674fee5175650d244512711eff43
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-02-05 15:46:28 +0600

    Switch statement's syntax

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+        $variableForSwitch = 100;
+
+        switch($variableForSwitch)
+        {
+            case 100:
+                echo "You are right!"; break;
+            case 50:
+                echo "This is 50"; break;
+            default:
+                echo "You've assigned another value";
+        }
+    
+?>
