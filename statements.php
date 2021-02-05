be2992e97eb582189c89296040d45b7107185644
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-02-05 15:40:56 +0600

    If-else statments and their conditions

---
+++ statements.php
@@ -0,0 +1,14 @@
+<?php
+    $varForStatement = 100;
+    if($varForStatment < 100)
+    {
+        $varForStatement += 1000;
+    }else if($varForStatement > 100)
+    {
+        $varForStatement += 100;
+    }else
+    {
+        $varForStatement += 100;
+    }
+    echo $varForStatement;
+?>
