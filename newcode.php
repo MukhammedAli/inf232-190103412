4502fb2c7a5a3d4288eb2c8fd32eeb33039b7381
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-02-06 00:20:31 +0600

    Printing array in reverse order by adding changes into newcode php

--- newcode.php
+++ newcode.php
@@ -1,13 +1,8 @@
 <?php
     $mass = array(1,2,3,4,5);
-    for($i = 0; $i < count($mass); $i++)
+    for($j = count($mass) - 1; $j > 0; $j--)
     {
-        if($mass[$i] % 2 == 0)
-        {
-            echo "Hi even number";
-        }else
-        {
-            echo "Hi odd number";
-        }
+        echo $mass[$j];
     }
+
 ?>
